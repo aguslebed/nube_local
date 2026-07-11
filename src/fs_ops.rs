e7b0b1b556240b8fec473d5e82fc7f1447bc2283
AgustinLebed <agus.lebed@gmail.com>
2026-07-11 19:56:37 -0300

    Funcion de crear carpeta

--- src/fs_ops.rs
+++ src/fs_ops.rs
@@ -5,8 +5,9 @@ Libreria que permite realizar todas las acciones que debe hacer la aplicacion.
 
 use std::path::PathBuf;
 
-pub fn create_new_dir(path: &PathBuf, name: &str) -> Result<(), std::io::Error> {
-    let full_path = path.join(name);
-    std::fs::create_dir(&full_path)?;
+pub fn create_new_dir(path: &PathBuf) -> Result<(), std::io::Error> {
+
+    println!("La direccion es: {:?}<-------------------", path);
+    std::fs::create_dir_all(&path)?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use std::path::PathBuf;
 use nube_local::fs_ops;
+use std::path::Path;
 
-const BASE_DIR: &str = "";
+const BASE_DIR_NAME: &str = "nube-data";
+
 fn get_root_path() -> PathBuf {
     dirs::home_dir()
         .expect("no se pudo encontrar el home")
-        .join("nube-data")
+        .join(BASE_DIR_NAME)
+}
+
+fn create_base_dir() -> Result<(), std::io::Error> {
+    let root_path = get_root_path();
+    std::path::Path::is_dir(&root_path).then(|| {
+        println!("El directorio base ya existe: {:?}", root_path);
+    }).unwrap_or_else(|| {
+        println!("Creando el directorio base: {:?}", root_path);
+        fs_ops::create_new_dir(&root_path.join(BASE_DIR_NAME)).expect("sadasd");  
+    });
+     Ok(())
 }
 
 fn main() {
+    create_base_dir().expect("Error al crear el directorio base");
     let root_path = get_root_path();
-    fs_ops::create_new_dir(&root_path, "test_dir").unwrap();
+    let full_path = root_path.join("test_dir");
+    fs_ops::create_new_dir(&full_path).unwrap();
     println!("Root path: {:?}", root_path);
 }
