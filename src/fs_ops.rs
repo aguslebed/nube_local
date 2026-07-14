3978d767b901ef2892dc04c61ad25e7fcfa32ae6
AgustinLebed <agus.lebed@gmail.com>
2026-07-14 19:21:20 -0300

    Funcion de eliminar y cambiar de nombre una carpeta.

--- src/fs_ops.rs
+++ src/fs_ops.rs
@@ -5,9 +5,37 @@ Libreria que permite realizar todas las acciones que debe hacer la aplicacion.
 
 use std::path::PathBuf;
 
-pub fn create_new_dir(path: &PathBuf) -> Result<(), std::io::Error> {
+pub fn create_new_dir(base_path: &PathBuf, dir_name: &str) -> Result<(), std::io::Error> {
+    let path = base_path.join(dir_name);
 
-    println!("La direccion es: {:?}<-------------------", path);
-    std::fs::create_dir_all(&path)?;
+    if path.is_dir() {
+        println!("El directorio ya existe: {:?}", path);
+    } else {
+        println!("Creando el directorio: {:?}", path);
+    }
+    std::fs::create_dir(&path)?;
+    Ok(())
+}
+
+pub fn delete_dir(base_path: &PathBuf, dir_name: &str) -> Result<(), std::io::Error> {
+    let full_path = base_path.join(dir_name);
+    if full_path.is_dir() { 
+    std::fs::remove_dir(&full_path)?;} 
+    else{
+        println!("El directorio no existe: {:?}", full_path);
+    }
+    Ok(())
+}
+
+pub fn change_dir_name(base_path: &PathBuf, old_name: &str, new_name: &str) -> Result<(), std::io::Error> {
+    let old_path = base_path.join(old_name);
+    let new_path = base_path.join(new_name);
+
+    if old_path.is_dir() {
+        std::fs::rename(&old_path, &new_path)?;
+        println!("Directorio renombrado de {:?} a {:?}", old_path, new_path);
+    } else {
+        println!("El directorio no existe: {:?}", old_path);
+    }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,25 @@ const BASE_DIR_NAME: &str = "nube-data";
 fn get_root_path() -> PathBuf {
     dirs::home_dir()
         .expect("no se pudo encontrar el home")
-        .join(BASE_DIR_NAME)
 }
 
 fn create_base_dir() -> Result<(), std::io::Error> {
     let root_path = get_root_path();
-    std::path::Path::is_dir(&root_path).then(|| {
+    if root_path.is_dir() {
         println!("El directorio base ya existe: {:?}", root_path);
-    }).unwrap_or_else(|| {
+    } else {
         println!("Creando el directorio base: {:?}", root_path);
-        fs_ops::create_new_dir(&root_path.join(BASE_DIR_NAME)).expect("sadasd");  
-    });
+        fs_ops::create_new_dir(&root_path, BASE_DIR_NAME).expect("Error al crear el directorio base");
+    }
      Ok(())
 }
 
 fn main() {
     create_base_dir().expect("Error al crear el directorio base");
-    let root_path = get_root_path();
-    let full_path = root_path.join("test_dir");
-    fs_ops::create_new_dir(&full_path).unwrap();
-    println!("Root path: {:?}", root_path);
+    let root_path = get_root_path().join(BASE_DIR_NAME);
+  
+    //fs_ops::create_new_dir(&root_path, "test_dir").unwrap();
+    //fs_ops::delete_dir(&root_path, "test_dir").unwrap();
+    fs_ops::change_dir_name(&root_path, "test_dir", "Carpeta de prueba").expect("Error al cambiar el nombre de la carpeta");
+    println!("Root path: {:?}", root_path.join("Carpeta de prueba"));
 }
