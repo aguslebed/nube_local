8dcb55aaab9b902277c8e3beef6989fe79c6117c
AgustinLebed <agus.lebed@gmail.com>
2026-07-15 18:59:58 -0300

    Agregue funcion de mover una carpeta de un directorio a otro.

--- src/fs_ops.rs
+++ src/fs_ops.rs
@@ -4,6 +4,7 @@ Libreria que permite realizar todas las acciones que debe hacer la aplicacion.
 */
 
 use std::path::PathBuf;
+use fs_more::directory::{self, DirectoryMoveOptions};
 
 pub fn create_new_dir(base_path: &PathBuf, dir_name: &str) -> Result<(), std::io::Error> {
     let path = base_path.join(dir_name);
@@ -37,5 +38,14 @@ pub fn change_dir_name(base_path: &PathBuf, old_name: &str, new_name: &str) -> R
     } else {
         println!("El directorio no existe: {:?}", old_path);
     }
+    Ok(())
+}
+
+pub fn move_dir(base_path: &PathBuf, dir_name: &str, new_path: &PathBuf) -> Result<(), std::io::Error>{
+    let old_path = base_path.join(dir_name);
+    let new_full_path = new_path.join(dir_name);
+    directory::move_directory(&old_path, &new_full_path, DirectoryMoveOptions::default())
+        .map(|_finished| ())
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,9 @@ fn main() {
     create_base_dir().expect("Error al crear el directorio base");
     let root_path = get_root_path().join(BASE_DIR_NAME);
   
-    //fs_ops::create_new_dir(&root_path, "test_dir").unwrap();
+    fs_ops::create_new_dir(&root_path, "test_dir").unwrap();
     //fs_ops::delete_dir(&root_path, "test_dir").unwrap();
-    fs_ops::change_dir_name(&root_path, "test_dir", "Carpeta de prueba").expect("Error al cambiar el nombre de la carpeta");
+    //fs_ops::change_dir_name(&root_path, "test_dir", "Carpeta de prueba").expect("Error al cambiar el nombre de la carpeta");
+    fs_ops::move_dir(&root_path, "Carpeta de prueba", &root_path.join("test_dir")).expect("Error al mover la carpeta");
     println!("Root path: {:?}", root_path.join("Carpeta de prueba"));
 }
